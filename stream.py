32d2855f0191ba5617096029f57abce734975f8f
yarosbaranov <yarosbaranov91@gmail.com>
2018-07-03 13:38:43 +0300

    to github!

---
+++ stream.py
@@ -0,0 +1,32 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = 'YOUR TOKEN'
+access_token_secret = "YOUR TOKEN Secret"
+consumer_key = "YOUR consumer Key"
+consumer_secret = "YOUR consumer key secret"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['EURef', 'brexit'])
